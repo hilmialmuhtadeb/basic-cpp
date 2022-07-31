b7e43959fa745be8c234f3aae193c6bd3223653e
Hilmi Almuhtade Billah <hilmialmuhtadeb@gmail.com>
2022-07-31 23:37:14 +0700

    learn for solve toki challenge

---
+++ seleksiOlimpiade/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t, n, m;
+    cin >> t >> n >> m;
+
+    while (t > 0) {
+        cout << "seleksi " << t << endl;
+        t--;
+    }
+}
